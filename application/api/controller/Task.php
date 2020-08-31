2a3b469b9674fc8ee54aa1f925410f40a91ee9a9
yc <yuchao@medlinker.com>
2020-08-31 18:05:57 +0800

    闯关奖励

--- application/api/controller/Task.php
+++ application/api/controller/Task.php
@@ -59,5 +59,96 @@ class Task extends Controller
         }
    }
 
+   /**
+    * 闯关活动
+    * 活动奖励结算
+    * 每天八点五分
+    */
+   public function passRewardSend(){
+       //获取今天八点结束挑战的活动
+       $date = date('Y-m-d');
+       $beginTime = $date.' 00:00:00';//今日凌晨
+       //八点五分时间
+       $endTime = $date.' 08:05:00';
+       $pass = db('pass')->where(['isEnd'=>0,'passEndTime'=>['<=',$endTime]])->select();
+       foreach($pass as $k => $v){
+           //判断当前状态
+           if($v['status'] == 1){
+               db('pass')->where('id',$v['id'])->update(['status'=>0]);
+           }
+           //获取所有报名信息
+           $allJoin = db('pass_join')->where(['passId'=>$v['id']])->select();
+           $userSign = [];//用户签到信息
+           $totalChallenge = count($allJoin);//挑战人数
+           $challengeSuccess = 0;//挑战成功人数
+           foreach($allJoin as $o => $p){
+               //判断报名挑战的挑战成功轮数
+               $signNumber = self::getUserSignNumber($p['uid'],$p['passId'],$p['id']);
+               //判断用户是否挑战成功 成功退还本金
+               $moneyReturn = self::getUserChallengeSuccess($p);
+               $userSign[] = [
+                   'uid'=>$p['uid'],
+                   'signNumber'=>$signNumber,
+                   'moneyReturn'=>$moneyReturn,//1—退本金 0-不退
+               ];
+               if($moneyReturn ==1){//退本金即挑战成功
+                   $challengeSuccess += 1;
+               }
+           }
+           //判断活动奖励模式 奖励类型 1-失败金额瓜分百分比 2-固定金额  3-报名百分比
+       }
+   }
+
+   /**
+    * 获取当前用户的挑战签到轮数
+    */
+   public static function getUserSignNumber($uid,$passId,$joinId){
+       if(!$uid || !$passId || !$joinId ){
+           return 0;
+       }
+       $hadSign = db('pass_sign')->where(['uid'=>$uid,'passId'=>$passId,'joinId'=>$joinId,'status'=>1])->order('number')->count();
+       if($hadSign){
+           return $hadSign;
+       }else{
+           return 0;
+       }
+   }
+   /**
+    * 判断用户当前挑战的状态
+    * status 参加状态  0-参与中 1-已完成 2-未完成
+    */
+   public static function getUserChallengeSuccess($join){
+       if(!$join || $join['status'] == 2){
+           return 0 ;// 0-失败 1-成功 退还本金
+       }
+       if($join['status'] == 1){
+           return 1;
+       }
+       if($join['status'] == 0){
+           //判断当前签到状态 参与签到状态 0-暂停 1-停止（挑战结束） 2-下一轮（继续挑战）
+           if($join['signStatus'] == 0 || $join['signStatus'] == 1){
+               return 1;
+           }else{//下一轮挑战中
+               //判断是否有未签到的记录
+               $noSign = db('pass_sign')->where(['uid'=>$join['uid'],'passId'=>$join['passId'],'joinId'=>$join['id'],'status'=>0])->find();
+               if($noSign){//存在 判断当前签到的结束时间 已过签到结束时间说明挑战失败 未过咋忽略该轮挑战签到  闯关暂停引起的
+                   $now = date('Y-m-d H:i:s');
+                   if($noSign['signTimeEnd'] >= $now){
+                       $status = 1;
+                       $success = 1;//1-退还本金 0-不退
+                   }else{//挑战失败
+                       $status = 2;
+                       $success = 0;
+                   }
+                    db('pass_join')->where('id',$join['id'])->update(['status'=>$status]);
+                   return $success;
+               }else{//没有 说明都签到成功
+                   db('pass_join')->where('id',$join['id'])->update(['status'=>1]);
+                   return 1;
+               }
+           }
+       }
+       return 0;
+   }
 
 }
